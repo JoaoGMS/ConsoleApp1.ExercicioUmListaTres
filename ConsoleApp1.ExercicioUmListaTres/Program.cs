47709a31aa8d2af25feba39eded31ceb2e6abeb6
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-21 13:46:28 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioUmListaTres/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioUmListaTres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] alturas = new double[15];
+
+            double maiorAltura = double.MinValue;
+            double menorAltura = double.MaxValue;
+
+            for (int i = 0; i < alturas.Length; i++)
+            {
+                Console.Write("Qual a altura? ");
+                string strAltura = Console.ReadLine();
+                alturas[i] = Convert.ToDouble(strAltura);
+
+                if (alturas[i] > maiorAltura)
+                    maiorAltura = alturas[i];
+                if (alturas[i] < menorAltura)
+                    menorAltura = alturas[i];
+            }
+
+            Console.WriteLine("A maior altura é: " + maiorAltura);
+            Console.WriteLine("A menor altura é: " + menorAltura);
+        }
+    }
+}
